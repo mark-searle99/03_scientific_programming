ff30df97c037e7253261c07763b69505d19a67be
Mark Searle <bg223@stud.uni-heidelberg.de>
2020-08-12 12:35:29 +0200

    created envelope test - Issue #1

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -15,4 +15,14 @@ def test_coordinates():
 
 
 def test_envelope():
-    assert False
+    # Given
+    coordinates = [(1, 1), (1, 2), (2, 2), (2, 1), (1, 1)]
+    # Test polygon
+    my_poly = MyPolygon(coordinates)
+    # Envelope of test polygon
+    my_poly_env = my_poly.envelope()
+    # Run tet with list comprehension, add all elements to list that are outside of envelope
+    test_list = [i for i in coordinates if i[0] < my_poly_env[0] or i[1] < my_poly_env[1] or i[0] > my_poly_env[2] or i[1] > my_poly_env[3]]
+    # Assert whether list is empty
+    assert not test_list
+
